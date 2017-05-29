404d7011b58778b5c4c993b28817a82c17b72d96
SpiderDave <ifallelsefailsusefire@gmail.com>
2017-05-29 17:54:37 -0400

    simplifying patch.bat in sample

--- samples/SMB/patch.bat
+++ samples/SMB/patch.bat
@@ -1,19 +1,2 @@
 @echo off
-rem ---- Configuration ----
-set patch=patch.txt
-rem -----------------------
-
-if %1X==makeX goto make
-if %1X==fixX goto theend
-goto :patch
-
-:make
-pushd ..
-call make
-popd
-echo make
-
-:patch
-..\..\davepatcher %patch%
-
-:theend
+..\..\davepatcher patch.txt
